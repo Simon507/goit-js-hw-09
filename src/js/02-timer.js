be77cb57cfe86dbf8315cbd5ef6b837a278148c9
Simon507 <Evgeny.klysak@gmail.com>
2022-12-15 22:52:21 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,18 +7,9 @@ const daysEl = document.querySelector('[data-days]');
 const hoursEl = document.querySelector('[data-hours]');
 const minutesEl = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
+DIFF_KEY = 'diff';
 
 let timer = null;
-// console.log('getFullYear(): ', thisDate.getFullYear());
-// console.log('getDate(): ', thisDate.getDate());
-
-// console.log('getMonth(): ', thisDate.getMonth() + 1);
-// console.log('getHours(): ', thisDate.getHours());
-// console.log('getMinutes(): ', thisDate.getMinutes());
-// console.log('getSeconds(): ', thisDate.getSeconds());
-// console.log('getMilliseconds(): ', thisDate.getMilliseconds());
-
-// console.log('Milliseconds(old): ', thisDate.getTime());
 
 const inputTime = document.getElementById('datetime-picker');
 
@@ -30,6 +21,8 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     const newDateMs = selectedDates[0].getTime();
+    console.log(newDateMs);
+    chekDate(newDateMs);
   },
 };
 
@@ -37,42 +30,71 @@ btnStart.setAttribute('disabled', 'disabled');
 
 flatpickr(inputTime, options);
 
-btnStart.addEventListener('click', calc);
-
-function calc() {
+function chekDate(newDateMs) {
   let thisDate = new Date();
   let thisDateMs = thisDate.getTime();
   let diff = newDateMs - thisDateMs;
+  localStorage.setItem(DIFF_KEY, diff);
   if (diff > 0) {
     btnStart.removeAttribute('disabled');
-    // timer = setInterval(() => {
-    //   thisDate = new Date();
-    //   thisDateMs = thisDate.getTime();
-    //   diff = newDateMs - thisDateMs;
-    //   convertMs(diff);
-    // }, 1000);
+    // getDifferent(diff);
   } else {
-    clearTimeout(timer);
+    btnStart.setAttribute('disabled', 'disabled');
     window.alert(`Please choose a date in the future`);
   }
 }
 
-// function convertMs(diff) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-//   // Remaining days
-//   const days = Math.floor(diff / day);
-//   // Remaining hours
-//   const hours = Math.floor((diff % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((diff % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((diff % day) % hour) % minute) / second);
-//   daysEl.textContent = days;
-//   hoursEl.textContent = hours;
-//   minutesEl.textContent = minutes;
-//   secondsEl.textContent = seconds;
-// }
+// console.log(diff);
+
+btnStart.addEventListener('click', onClick);
+
+function onClick() {
+  timer = setInterval(() => {
+    const getDifferent = function (diff) {
+      console.log(diff);
+    };
+    console.log(getDifferent);
+    const different = localStorage.getItem('diff');
+    convertMs(different);
+  }, 1000);
+}
+
+// btnStart.addEventListener('click', calc);
+
+// // function calc() {
+// //   let thisDate = new Date();
+// //   let thisDateMs = thisDate.getTime();
+// //   let diff = newDateMs - thisDateMs;
+// //   if (diff > 0) {
+// //     btnStart.removeAttribute('disabled');
+// //     // timer = setInterval(() => {
+// //     //   thisDate = new Date();
+// //     //   thisDateMs = thisDate.getTime();
+// //     //   diff = newDateMs - thisDateMs;
+// //     //   convertMs(diff);
+// //     // }, 1000);
+// //   } else {
+// //     clearTimeout(timer);
+// //     window.alert(`Please choose a date in the future`);
+// //   }
+// // }
+
+function convertMs(different) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  // Remaining days
+  const days = Math.floor(different / day);
+  // Remaining hours
+  const hours = Math.floor((different % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((different % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((different % day) % hour) % minute) / second);
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minutesEl.textContent = minutes;
+  secondsEl.textContent = seconds;
+}
