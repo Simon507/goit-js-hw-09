1d5a2f59734557cb4c0964a5a1daff3ebf147b67
Simon507 <Evgeny.klysak@gmail.com>
2022-12-19 22:37:50 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,28 +9,35 @@ form.addEventListener('submit', startPromise);
 
 function startPromise(evt) {
   evt.preventDefault();
+  let promiseValue = {
+    position: '',
+    delay: '',
+  };
   let delay = Number(delayEl.value);
   for (let i = 1; i <= amountEl.value; i++) {
     const position = i;
-    // console.log(position);
-    // console.log(delay);
-    createPromise(position, delay);
+    promiseValue.position = position;
+    promiseValue.delay = delay;
+    console.log(promiseValue);
+
+    createPromise(promiseValue);
+
     delay = delay + Number(stepEl.value);
   }
 }
 
-function createPromise(position, delay) {
+function createPromise(promiseValue) {
   const shouldResolve = Math.random() > 0.3;
 
   setTimeout(() => {
     if (shouldResolve) {
       // Fulfill
-      Notify.success(`Promise # ${position} is OK`);
-      // console.log(position);
-      // console.log(delay);
+      Notify.success(`Promise # ${promiseValue.position} is OK`);
+      console.log(`delay is ${promiseValue.delay}`);
     } else {
-      Notify.failure(`Promise # ${position} is'nt OK`);
+      Notify.failure(`Promise # ${promiseValue.position} is'nt OK`);
+      console.log(`delay is ${promiseValue.delay}`);
       // Reject
     }
-  }, delay);
+  }, promiseValue.delay);
 }
